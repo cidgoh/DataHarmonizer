992fa038e8ff4eaa494fc3e451d17ef133bc97da
Damion Dooley <damion.dooley@bccdc.ca>
2020-05-22 11:25:38 -0700

    various functionality (#3)
    * various functionality
    
    - popup alert of field info when click on table TH
    - validation of numeric and date fields
    - frozen left pane of id field
    - multiple country lists displayed.
    
    * fix for first load/render and th coloration
    
    * change th click to dblclick
    
    * help text for fields
    
    * tweak to ease off validation on autocomplete
    
    so values with spaces in front don't trigger errors on selection
    
    * 2 char tabs
    
    * unused css
    
    * jquery to launch header help
    
    * Reformat
    
    * wip css
    
    * wip css
    
    * revised make_data.py and main.js
    
    * wip css
    
    * format data.js
    
    * Format data.js
    
    * format data.js
    
    * Cleaned up some css and js
    
    * Cleaned up some jsdoc
    
    * Clean up validateGrid
    
    * Remove stale comments and code
    
    * Clean up field comments
    
    * Now use modal
    
    * Also introduce `getFields` for compact code
    
    * Stop using handsontable's native validation
    
    Co-authored-by: Ivan Sohrab Gill <ivan.gill@bccdc.ca>

--- make_data.py
+++ make_data.py
@@ -2,48 +2,79 @@
 import csv
 import json
 import collections
+import dpath.util
 
 r_filename = 'data.tsv'
 w_filename = 'data.js'
 DATA = []
 FIELD_INDEX = {}
-
+CHOICE_INDEX = {} # [choice] -> [parent choice or field]
 section = None
 
+'''
+Creates section / field / field choice data structure directly off of tabular
+data captured from a vocabulary spreadsheet.
+'''
 with open(r_filename) as tsvfile:
   reader = csv.DictReader(tsvfile, dialect='excel-tab')
   for row in reader:
     # row has keys 'Ontology ID' 'parent class' 'label' 'datatype' 'requirement'
 
     # Skip second row (a robot directive row)
-    if len(row['Ontology ID']) == 0 or row['Ontology ID'] != 'ID': 
-    	if row['label'] > '':
+    if len(row['Ontology ID']) == 0 or row['Ontology ID'] != 'ID':
+    	label = row['label'].strip()
+    	if label > '':
 	    	if row['parent class'] == '': 
-	    		section = {'fieldName': row['label'], 'children': []}
+	    		# Define a section of fields
+	    		section = {'fieldName': label, 'children': []}
 	    		children = section['children']
 	    		DATA.append(section)
+
 	    	else:
-	    		# find parent class in DATA or in index of its fields
+	    		# Find parent class in DATA or in index of its fields
 	    		parent_label = row['parent class']
-	    		section = next((x for x in DATA if x['fieldName'] == parent_label), None)
+	    		section = next((x for x in DATA if x['fieldName'].strip() == parent_label), None)
 	    		if section:
 	    			field = {
-	    				'fieldName': row['label'], 
-	    				'datatype': row['datatype'],
+	    				'fieldName':   label, 
+	    				'ontology_id': row['Ontology ID'],
+	    				'datatype':    row['datatype'],
 	    				'requirement': row['requirement'],
-	    				'vocabulary': collections.OrderedDict()
+	    				'description': row['description'],
+	    				'guidance':    row['guidance'],
+	    				'examples':    row['examples']
 	    			}
+	    			if row['datatype'] == 'select' or row['datatype'] == 'multiple':
+	    				choice = collections.OrderedDict();
+	    				# Case sensitive index, curators must be exact
+	    				CHOICE_INDEX[label] = choice; 
+	    				field['vocabulary'] = choice;
+
 	    			section['children'].append(field)
-	    			FIELD_INDEX[row['label'].lower()] = field
+	    			FIELD_INDEX[label.lower()] = field
+
 	    		else:
-	    			# term parent isn't a section so see if it is a field
-	    			if parent_label.lower() in FIELD_INDEX:
-	    				FIELD_INDEX[parent_label.lower()]['vocabulary'][row['label']] = {}
+	    			parent_label_lc = parent_label.lower()
+	    			# Item isn't a section or field, so it must be a select field choice
+	    			# find the choice's parent in FIELD_INDEX, if any
+	    			# If parent in CHOICE_INDEX, then add it
+
+	    			if parent_label_lc in FIELD_INDEX:
+	    				choice = collections.OrderedDict();
+	    				if not 'vocabulary' in FIELD_INDEX[parent_label_lc]:
+	    					print ("error: field ",parent_label, "not marked as select or multiple but it has child", label)
+	    				else:
+	    					FIELD_INDEX[parent_label_lc]['vocabulary'][label] = choice;
+	    					CHOICE_INDEX[label] = choice;
 	    			else:
-	    				print ("Error: parent class for term", row['label'], "doesn't exist as section or field:", parent_label)
+	    				try:
+	    					result = dpath.util.get(CHOICE_INDEX, '*/' + parent_label, separator='/')
+	    					result[label] = collections.OrderedDict(); # Add new child
+	    				except:
+	    					print ("Error: parent class ", parent_label, "doesn't exist as section or field for term", label)
 
 
 with open(w_filename, 'w') as output_handle:
 	# DO NOT USE sort_keys=True on piclists etc. because this overrides
 	# OrderedDict() sort order.
-	output_handle.write("const DATA = " + json.dumps(DATA, sort_keys = False, indent = 4, separators = (',', ': ')))
+	output_handle.write("const DATA = " + json.dumps(DATA, sort_keys = False, indent = 2, separators = (',', ': ')))
