7a44191e71a2e40c292cdf582303cfdfb068a6b7
Damion Dooley <damion.dooley@bccdc.ca>
2020-12-03 12:55:29 -0800

    version bump

--- script/main.js
+++ script/main.js
@@ -11,7 +11,7 @@
  * main.html?template=test_template
  *
  */
-const VERSION = '0.13.7';
+const VERSION = '0.13.8';
 const TEMPLATES = {
   'CanCOGeN Covid-19': {'folder': 'canada_covid19', 'status': 'published'},
   'PHAC Dexa (ALPHA)': {'folder': 'phac_dexa', 'status': 'draft'},
