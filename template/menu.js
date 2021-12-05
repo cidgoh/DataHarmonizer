a04d49c474d926b910f7d024226c2bfa2e60ceb5
Damion Dooley <damion.dooley@bccdc.ca>
2021-12-05 12:34:41 -0800

    menu.js fix for existing unwanted entries

--- template/menu.js
+++ template/menu.js
@@ -1,282 +1,142 @@
 const TEMPLATES = {
-  "MIxS agriculture": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS food-farm environment": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS food-food production facility": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS food-animal and animal feed": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS food-human foods": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS symbiont-associated": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS water": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS wastewater_sludge": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS soil": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS sediment": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS plant-associated": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS miscellaneous natural or artificial environment": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS microbial mat_biofilm": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS hydrocarbon resources-fluids_swabs": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS hydrocarbon resources-cores": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS human-vaginal": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS human-skin": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS human-oral": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS human-gut": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS human-associated": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS host-associated": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS built environment": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS air": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS core": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIGS eukaryote": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIGS bacteria": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIGS plant": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIGS virus": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIGS org": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIMS": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIMARKS specimen": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIMARKS survey": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MISAG": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIMAG": {
-    "folder": "MIxS",
-    "status": "published"
-  },
-  "MIxS MIUVIG": {
-    "folder": "MIxS",
-    "status": "published"
-  },
   "MIxS/agriculture": {
-    "folder": "MIxS/agriculture",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/food-farm environment": {
-    "folder": "MIxS/food-farm environment",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/food-food production facility": {
-    "folder": "MIxS/food-food production facility",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/food-animal and animal feed": {
-    "folder": "MIxS/food-animal and animal feed",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/food-human foods": {
-    "folder": "MIxS/food-human foods",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/symbiont-associated": {
-    "folder": "MIxS/symbiont-associated",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/water": {
-    "folder": "MIxS/water",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/wastewater_sludge": {
-    "folder": "MIxS/wastewater_sludge",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/soil": {
-    "folder": "MIxS/soil",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/sediment": {
-    "folder": "MIxS/sediment",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/plant-associated": {
-    "folder": "MIxS/plant-associated",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/miscellaneous natural or artificial environment": {
-    "folder": "MIxS/miscellaneous natural or artificial environment",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/microbial mat_biofilm": {
-    "folder": "MIxS/microbial mat_biofilm",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/hydrocarbon resources-fluids_swabs": {
-    "folder": "MIxS/hydrocarbon resources-fluids_swabs",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/hydrocarbon resources-cores": {
-    "folder": "MIxS/hydrocarbon resources-cores",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/human-vaginal": {
-    "folder": "MIxS/human-vaginal",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/human-skin": {
-    "folder": "MIxS/human-skin",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/human-oral": {
-    "folder": "MIxS/human-oral",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/human-gut": {
-    "folder": "MIxS/human-gut",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/human-associated": {
-    "folder": "MIxS/human-associated",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/host-associated": {
-    "folder": "MIxS/host-associated",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/built environment": {
-    "folder": "MIxS/built environment",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/air": {
-    "folder": "MIxS/air",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/core": {
-    "folder": "MIxS/core",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIGS eukaryote": {
-    "folder": "MIxS/MIGS eukaryote",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIGS bacteria": {
-    "folder": "MIxS/MIGS bacteria",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIGS plant": {
-    "folder": "MIxS/MIGS plant",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIGS virus": {
-    "folder": "MIxS/MIGS virus",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIGS org": {
-    "folder": "MIxS/MIGS org",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIMS": {
-    "folder": "MIxS/MIMS",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIMARKS specimen": {
-    "folder": "MIxS/MIMARKS specimen",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIMARKS survey": {
-    "folder": "MIxS/MIMARKS survey",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MISAG": {
-    "folder": "MIxS/MISAG",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIMAG": {
-    "folder": "MIxS/MIMAG",
+    "folder": "MIxS",
     "status": "published"
   },
   "MIxS/MIUVIG": {
-    "folder": "MIxS/MIUVIG",
+    "folder": "MIxS",
     "status": "published"
   }
 }
