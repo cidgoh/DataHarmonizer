1566feaa62e4ee9ecf6ea66f5d96cd5933b97baf
Damion Dooley <damion.dooley@bccdc.ca>
2022-02-24 21:47:34 -0800

    version bump

--- script/main.js
+++ script/main.js
@@ -11,7 +11,7 @@
  * main.html?template=test_template
  *
  */
-const VERSION = '0.15.1';
+const VERSION = '0.15.2';
 const VERSION_TEXT = 'DataHarmonizer provenance: v' + VERSION;
 const TEMPLATES = {
   'CanCOGeN Covid-19': {'folder': 'canada_covid19', 'status': 'published'},
