abc8f77fd5a01bf139adf64f046876a4cd6f6b3b
Damion Dooley <damion.dooley@bccdc.ca>
2022-05-05 23:20:01 -0700

    conversion script for replacing old data.tsv versions

--- Makefile
+++ Makefile
@@ -1,17 +1,20 @@
 DEEP_PREF=../..
 CONVERSION_SCRIPT=script/linkml.py
 
-.PHONY: all clean mix nmdc
+.PHONY: all clean mixs nmdc canada_covid19
 
-all: clean template/MIxS/schema.js template/nmdc_dh/schema.js
+all: clean template/*/schema.js
 mixs: clean template/MIxS/schema.js
 nmdc: clean template/nmdc_dh/schema.js
+canada_covid19: clean template/canada_covid19/schema.js
 
 clean:
 	rm -rf template/menu.js
+
 	rm -rf template/MIxS/schema.js
 	rm -rf template/nmdc_dh/schema.js
 	rm -rf template/nmdc_dh/source/nmdc_dh.yaml
+	rm -rf template/canada_covid19/schema.js
 
 template/nmdc_dh/source/nmdc_dh.yaml:
 	wget -O $@ https://raw.githubusercontent.com/microbiomedata/sheets_and_friends/main/artifacts/nmdc_dh.yaml
@@ -22,6 +25,11 @@ template/MIxS/schema.js: template/MIxS/source/mixs.yaml
 	# now open linkml.html in a web browser
 
 template/nmdc_dh/schema.js: template/nmdc_dh/source/nmdc_dh.yaml
+	$(eval DIRNAME=$(shell dirname $@))
+	cd $(DIRNAME) ; python3 $(DEEP_PREF)/$(CONVERSION_SCRIPT) --input $(DEEP_PREF)/$<
+	# now open linkml.html in a web browser
+
+template/canada_covid19/schema.js: template/canada_covid19/schema.yaml
 	$(eval DIRNAME=$(shell dirname $@))
 	cd $(DIRNAME) ; python3 $(DEEP_PREF)/$(CONVERSION_SCRIPT) --input $(DEEP_PREF)/$<
 	# now open linkml.html in a web browser
---
+++ script/tabular_to_schema.py
@@ -0,0 +1,226 @@
+#
+# tabular_to_schema.py: A script to generate a LinkML yaml schema from a 
+# tabular representation of the schema data structure.
+#
+# Author: Damion Dooley
+# 
+
+import csv
+import yaml
+from functools import reduce
+
+r_schema_core = 'schema_core.yaml';
+r_schema_slots = 'schema_slots.tsv';
+r_schema_enums = 'schema_enums.tsv';
+w_filename = 'schema.yaml';
+
+with open(r_schema_core, 'r') as file:
+	SCHEMA = yaml.safe_load(file)
+
+'''
+settings: not allowed yet.
+  lower case: "^[a-z\\W\\d_]*$"
+  UPPER CASE: "^[A-Z\\W\\d_]*$"
+  Title Case: "^(((?<=\b)[^a-z\\W]\\w*?|[\\W])+)$"
+'''
+'''
+Process each slot given in tabular format.
+'''
+EXPORT_FORMAT = [];
+
+with open(r_schema_slots) as tsvfile:
+	reader = csv.DictReader(tsvfile, dialect='excel-tab');
+
+    # Row has keys: specification slot_uri	is_a	title	range	range_2	identifier	
+    # multivalued	required	recommended	minimum_value	maximum_value	
+    # capitalize	pattern	description	comments	examples	...
+
+	firstrow = True;
+	rank = 1;
+
+	for row in reader:
+
+		# Cleanup of cell contents.
+		for field in row:
+			row[field] = row[field].strip();
+
+		if row.get('class_name','') > '':
+			schema_class = SCHEMA['classes'][row.get('class_name')];
+			schema_class['slots'] = []
+			schema_class['slot_usage'] = {}
+
+		# Get list of slot (field) export mappings. Each has "EXPORT_" 
+		# prefixed into it.
+		if (firstrow):
+			firstrow = False;
+			for key in row:
+				if key[0:7] == 'EXPORT_':
+					EXPORT_FORMAT.append(key);
+
+		# All slots have a range
+		if row.get('range','') > '':
+
+			label = row.get('name',False) or row.get('title','[UNNAMED!!!]')
+
+			print ("processing SLOT:", label)
+
+			#Append this slot:
+			slot = {
+				'name': label
+			}
+
+			# from_schema
+			# owner
+
+			if row.get('title','') > '':
+				slot['title'] = row['title'];
+
+			if row.get('description','') > '':
+				slot['description'] = row['description'];
+			if row.get('comments','') >'':
+				slot['comments'] = row['comments'];
+			if row.get('examples','') > '':
+				slot['examples'] = [{'value': v} for v in row['examples'].split(';')];
+
+			if row.get('slot_uri','') > '':
+				slot['slot_uri'] = row['slot_uri'];
+
+			if row.get('range','') > '':
+				#if row.get('range_2','') > '':
+				slot['range'] = row['range'];		
+
+			if row.get('slot_uri',False) == True:
+				slot['identifier'] = True;
+			if row.get('multivalued',False) == True:
+				slot['multivalued'] = True;
+			if row.get('required',False) == True:
+				slot['required'] = True;
+			if row.get('recommended', False) == True:
+				slot['recommended'] = True;
+
+			# NEED TO FIX TO ACCEPT Dates, and "{today}"
+			if row.get('minimum_value','') > '':
+				if row['minimum_value'].isnumeric():
+					slot['minimum_value'] = row['minimum_value'];
+			if row.get('maximum_value','') > '':
+				if row['maximum_value'].isnumeric():
+					slot['maximum_value'] = row['maximum_value'];
+
+			if row.get('pattern','') > '':
+				slot['pattern'] = row['pattern'];				
+			if row.get('string_serialization','') > '':
+				slot['string_serialization'] = row.get('string_serialization')
+
+			if len(EXPORT_FORMAT) > 0:
+				mappings = []
+				for export_field in EXPORT_FORMAT:
+					if row[export_field] > '':
+						prefix = export_field[7:] + ':'
+						# Can be multiple targets for an exportable field
+						for value in row[export_field].split(';'):
+							mappings.append(prefix + value)
+
+				if len(mappings) > 0:
+					slot['exact_mappings'] = mappings
+
+			SCHEMA['slots'][slot['name']] = slot;
+
+			schema_class['slots'].append(slot['name'])
+
+			#### Now add particular slot_usage requirements
+			slot_usage = {
+				#'name': label,
+				'rank': rank
+			}
+			rank += 1;
+			if row.get('slot_group','') > '':
+				slot_usage['slot_group'] = row['slot_group'];
+
+			schema_class['slot_usage'][slot['name']] = slot_usage
+
+'''
+Process each enumeration provided in tabular tsv format.
+'''
+with open(r_schema_enums) as tsvfile:
+	reader = csv.DictReader(tsvfile, dialect='excel-tab');
+
+    # Row has keys: meaning	title	permissable_values	permissable_values_2	permissable_values_3	permissable_values_4	permissable_values_5	description		...   EXPORT_GISAID	EXPORT_CNPHI	EXPORT_NML_LIMS	EXPORT_BIOSAMPLE	EXPORT_VirusSeq_Portal
+
+	enumerations = SCHEMA['enums']
+
+	rank = 1;
+	title = ''; # running title for chunks of enumeration rows
+	menu_path = []
+	enum = {}
+
+	for row in reader:
+
+		for field in row:
+			row[field] = row[field].strip();
+
+		if row.get('title','') > '':
+			title = row.get('title');
+
+			print("processing ENUMERATION:", title)
+
+			if not (title in enumerations):
+				enum = {
+					'name': title,
+					# from_schema: ...
+					'permissible_values': {}
+				}
+				enumerations[title] = enum
+
+		# Text is label of a particular menu choice
+		text = '';
+		for depth in range(1,6):
+			column = 'menu_' + str(depth)
+
+			if row.get(column,'') > '':
+				text = row.get(column);
+				del menu_path[depth-1:] # Menu path always points to parent
+				break;
+
+		# Here there is a menu item to process
+		if text > '':
+			choice = {
+				'text' : text
+			}
+
+			search_path = menu_path.copy()
+			# insert permissible_values in between menu item depth
+			for x in range(len(search_path)):
+				search_path.insert(2*x, 'permissible_values')
+
+			parent = reduce(dict.get, search_path, enum);
+
+			if row.get('description','') > '':
+				choice['description'] = row.get('description');
+
+			if row.get('meaning','') > '':
+				choice['meaning'] = row.get('meaning');
+
+			# Add choice dependency relations on other choices here
+
+			if not ('permissible_values' in parent):
+				parent['permissible_values'] = {}
+
+			if len(EXPORT_FORMAT) > 0:
+				mappings = []
+				for export_field in EXPORT_FORMAT:
+					if row[export_field] > '':
+						prefix = export_field[7:] + ':'
+						for value in row[export_field].split(';'):
+							mappings.append(prefix + value)
+
+				if len(mappings) > 0:
+					choice['exact_mappings'] = mappings
+
+			parent['permissible_values'][text] = choice
+			menu_path.append(text)
+
+
+with open(w_filename, 'w') as output_handle:
+	yaml.dump(SCHEMA, output_handle, sort_keys=False)
+
+
