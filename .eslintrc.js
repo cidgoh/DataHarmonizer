5f11f3cc569d130f0f69279ae32f1ec272dd5466
Patrick Kalita <pkalita@lbl.gov>
2022-06-15 11:47:17 -0700

    Add eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "ignorePatterns": [
+        "main.js",
+        "**/dist/*.js"
+    ],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
