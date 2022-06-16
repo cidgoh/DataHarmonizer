dc594bbb65be71062e3c1cc63b79e87256ad1467
Patrick Kalita <pkalita@lbl.gov>
2022-06-16 11:49:23 -0700

    Create DH instance with static schema on local site

--- web/index.js
+++ web/index.js
@@ -1,9 +1,16 @@
-import $ from 'jquery';
+import { DataHarmonizer } from '#lib';
+import monkeypox from './templates/monkeypox/schema.json';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-$(function () {
-  $('#data-harmonizer-toolbar').html('toolbar');
-  $('#data-harmonizer-grid').html('grid');
+document.addEventListener('DOMContentLoaded', function () {
+  const dhRoot = document.querySelector('#data-harmonizer-grid');
+  const dhFooterRoot = document.querySelector('#data-harmonizer-footer');
+
+  const dh = new DataHarmonizer(dhRoot, dhFooterRoot, {
+    schema: monkeypox,
+    loadingScreenRoot: document.querySelector('body'),
+  });
+  dh.processTemplate('Monkeypox');
 });
