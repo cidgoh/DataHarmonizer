817b51fec8587bd3c131fd0101ea2dcf764e27a9
Patrick Kalita <pkalita@lbl.gov>
2022-06-17 14:16:13 -0700

    Fix Getting Started modal

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -25,6 +25,10 @@ module.exports = (env, argv) => {
           exclude: path.resolve(__dirname, 'index.html'),
           type: 'asset/source',
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
     devServer: {
