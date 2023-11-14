a4a64ce17e5dd0836cbfadee1bf8755d2e5fe57d
Kenneth Bruskiewicz <kbruskie@sfu.ca>
2023-11-14 12:28:19 -0800

    HOTFIX: schema slot resources weren't adding properly, i18n doesn't support nesting by default. Need to rework i18n IDs

--- web/index.js
+++ web/index.js
@@ -2,7 +2,8 @@ import * as $ from 'jquery';
 import i18n from 'i18next';
 import { DataHarmonizer, Footer, Toolbar } from '@/lib';
 import { initI18n } from '@/lib/utils/i18n';
-import { Template } from '@/lib/utils/templates';
+import { Template, findBestLocaleMatch } from '@/lib/utils/templates';
+import { flattenObject } from '@/lib/utils/objects';
 
 import menu from '@/web/templates/menu.json';
 import tags from 'language-tags';
@@ -21,7 +22,9 @@ document.addEventListener('DOMContentLoaded', function () {
   // internationalize
   // TODO: connect to locale of schema!
   // Takes `lang` as argument (unused)
-  initI18n(() => {
+  initI18n((lang) => {
+    console.log(lang);
+    console.log(i18n);
     $(document).localize();
     dh.hot.render();
   });
@@ -68,7 +71,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
       Object.entries(template.translations).forEach(
         ([langcode, translation]) => {
-          i18n.addResources(langcode, 'translations', translation.schema);
+          // schema_resource:
+          // slots -> <slot_symbol> -> <slot_name> ~ label
+          // :: translation.schema.slots
+          // enums
+
+          const schema_resource = Object.entries(translation.schema.slots)
+            .reduce((acc, [slot_symbol, { name }]) => ({ ...acc, [slot_symbol.replace(/ /g, '_')]: name }), {});
+          
+            console.log([langcode, translation.schema.slots]);
+          console.log(schema_resource);
+
+          // TODO: namespace to schema path?
+          i18n.addResources(langcode.split('-')[0], 'translation', schema_resource);
         }
       );
 
