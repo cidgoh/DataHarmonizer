e6665e0a87ba2bdab962a9c52b0af25361604cb0
Kenneth Bruskiewicz <kbruskie@sfu.ca>
2023-12-05 15:50:46 -0800

    Copy Paste internationalization semantics. Preserve internationalization in column, leave blank otherwise

--- web/index.js
+++ web/index.js
@@ -23,8 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Takes `lang` as argument (unused)
   initI18n((lang) => {
     console.log(lang);
-    //dh.hot.render();
-    console.log('localizing in render')
+    // dh.hot.render();
     $(document).localize();
   });
 
