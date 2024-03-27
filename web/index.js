25c8b5e842ff1a5bdca70e936771a3ef8386c3b4
Kenneth Bruskiewicz <kbruskie@sfu.ca>
2024-03-27 12:00:39 -0700

    debug validation in multi table case

--- web/index.js
+++ web/index.js
@@ -93,7 +93,8 @@ class AppContext {
     }
 
     getCurrentDataHarmonizer() {
-      return this.dhs[this.current_data_harmonizer_name]
+        console.log(this.current_data_harmonizer_name, this.dhs[this.current_data_harmonizer_name])
+        return this.dhs[this.current_data_harmonizer_name]
     }
 
     // TODO: memoize?
