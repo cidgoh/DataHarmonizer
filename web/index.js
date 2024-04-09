f4041af9019bb8abd525110084d9acd54dbdb7bd
Kenneth Bruskiewicz <kbruskie@sfu.ca>
2024-04-09 16:40:48 -0700

    prettier

--- web/index.js
+++ web/index.js
@@ -68,12 +68,10 @@ class AppConfig {
 }
 
 class AppContext {
-
   schema_tree = {};
   dhs = {};
   current_data_harmonizer_name = null;
   template = null;
-  
 
   constructor(appConfig) {
     this.appConfig = appConfig;
@@ -565,7 +563,6 @@ function findSlotNamesForClass(schema, class_name) {
  * @returns {Object|null} The schema tree object, or null if no "Container" classes are found.
  */
 function buildSchemaTree(schema) {
-
   function updateChildrenAndSharedKeys(data) {
     // Use a deep clone to avoid mutating the original object
     const result = JSON.parse(JSON.stringify(data));
@@ -635,13 +632,12 @@ function buildSchemaTree(schema) {
         });
       }, {}),
     };
+  }
 
-  };
-
-  console.log('has Container')
+  console.log('has Container');
 
   const classes = Object.keys(schema.classes).filter(
-    (el) => el !== 'dh_interface' && el !== 'Container' 
+    (el) => el !== 'dh_interface' && el !== 'Container'
   );
 
   const tree_base = {
@@ -660,7 +656,7 @@ function buildSchemaTree(schema) {
   }, tree_base);
 
   const schema_tree = updateChildrenAndSharedKeys(pre_schema_tree);
-  console.log(schema_tree)
+  console.log(schema_tree);
   return schema_tree;
 }
 
@@ -693,7 +689,6 @@ function makeDataHarmonizersFromSchemaTree(
   schema_name,
   export_formats
 ) {
-
   function createDataHarmonizerContainer(dhId, isActive) {
     let dhSubroot = document.createElement('div');
     dhSubroot.id = dhId;
@@ -741,11 +736,10 @@ function makeDataHarmonizersFromSchemaTree(
           dhTab.addEventListener('click', () => {
             // set the current dataharmonizer tab in the context
             context.setCurrentDataHarmonizer(spec.name);
-            
-            $(document).trigger('dhCurrentChange', {
-              data: spec.name
-            });
 
+            $(document).trigger('dhCurrentChange', {
+              data: spec.name,
+            });
           });
           dhTabNav.appendChild(dhTab); // Appending to the tab navigation
 
@@ -900,16 +894,13 @@ function setupSharedColumn(data_harmonizer, shared_key_name, callback) {
  */
 function makeSharedKeyHandler(data_harmonizer, schema_tree_node) {
   const makeUpdateHandler = (shared_key_spec) => {
-
     const updateSchemaNodeChildrenCallback = (
       changes,
       source,
       old_value,
       new_value
     ) => {
-
       schema_tree_node.children.forEach((cls_key) => {
-
         // lift this out to a more general function?
 
         // transformation handler: what to do when a cell with a shared key is updated
@@ -928,12 +919,10 @@ function makeSharedKeyHandler(data_harmonizer, schema_tree_node) {
         //         visitSchemaTree(schema_tree, () => transformMultivaluedColumn(data_harmonizers[cls_key], shared_key_name, changes, source, old_value, new_value), cls_key)
         //     })
         // }, cls_key);
-
       });
     };
 
     return updateSchemaNodeChildrenCallback;
-
   };
 
   schema_tree_node.shared_keys.forEach((shared_key_spec) => {
@@ -980,30 +969,31 @@ function attachPropagationEventHandlersToDataHarmonizers(
     }
   });
 
-
   function stripDiv(html) {
-    const div = document.createElement("div");
+    const div = document.createElement('div');
     div.innerHTML = html;
-    return (div.innerText);
+    return div.innerText;
   }
 
-  Object.values(data_harmonizers).forEach(dh => {
-
+  Object.values(data_harmonizers).forEach((dh) => {
     dh.hot.addHook('afterSelection', (row, col) => {
       const valueToMatch = dh.hot.getDataAtCell(row, col);
 
       // get value at cell
       // filter other data harmonizer at cell
-      schema_tree[dh.class_assignment].children.forEach(child_name => {
-        const shared_key_name = schema_tree[dh.class_assignment].shared_keys.filter(el => el.related_concept === child_name)[0].name;
+      schema_tree[dh.class_assignment].children.forEach((child_name) => {
+        const shared_key_name = schema_tree[
+          dh.class_assignment
+        ].shared_keys.filter((el) => el.related_concept === child_name)[0].name;
         visitSchemaTree(
           schema_tree,
           (schema_tree_node) => {
-            
             const hot = data_harmonizers[schema_tree_node.name].hot;
             const columnHeaders = hot.getColHeader();
             const columnName = shared_key_name; // shared_key based on event selection ~ replace columnIndex with event data?
-            const columnIndex = columnHeaders.map(stripDiv).findIndex(header => header === columnName);
+            const columnIndex = columnHeaders
+              .map(stripDiv)
+              .findIndex((header) => header === columnName);
 
             if (columnIndex === -1) {
               console.error('Column name not found');
@@ -1011,49 +1001,48 @@ function attachPropagationEventHandlersToDataHarmonizers(
             }
             const plugin = hot.getPlugin('filters');
             // Add a condition where the column value equals the specified value
-            plugin.clearConditions(columnIndex);  // change valueToMatch per new selection in the column
+            plugin.clearConditions(columnIndex); // change valueToMatch per new selection in the column
             plugin.addCondition(columnIndex, 'eq', [valueToMatch]);
             plugin.filter();
-
           },
-          child_name)
-      })
+          child_name
+        );
+      });
     });
 
     dh.hot.addHook('afterDeselect', () => {
+      // get value at cell
+      // filter other data harmonizer at cell
+      schema_tree[dh.class_assignment].children.forEach((child_name) => {
+        const shared_key_name = schema_tree[
+          dh.class_assignment
+        ].shared_keys.filter((el) => el.related_concept === child_name)[0].name;
 
-        // get value at cell
-        // filter other data harmonizer at cell
-        schema_tree[dh.class_assignment].children.forEach(child_name => {
+        visitSchemaTree(
+          schema_tree,
+          (schema_tree_node) => {
+            const hot = data_harmonizers[schema_tree_node.name].hot;
+            const columnHeaders = hot.getColHeader();
+            const columnName = shared_key_name; // shared_key based on event selection ~ replace columnIndex with event data?
+            const columnIndex = columnHeaders
+              .map(stripDiv)
+              .findIndex((header) => header === columnName);
 
-          const shared_key_name = schema_tree[dh.class_assignment].shared_keys.filter(el => el.related_concept === child_name)[0].name;
+            if (columnIndex === -1) {
+              console.error('Column name not found');
+              return;
+            }
 
-          visitSchemaTree(
-            schema_tree,
-            (schema_tree_node) => {
-
-              const hot = data_harmonizers[schema_tree_node.name].hot;
-              const columnHeaders = hot.getColHeader();
-              const columnName = shared_key_name; // shared_key based on event selection ~ replace columnIndex with event data?
-              const columnIndex = columnHeaders.map(stripDiv).findIndex(header => header === columnName);
-  
-              if (columnIndex === -1) {
-                console.error('Column name not found');
-                return;
-              }
-
-              const plugin = hot.getPlugin('filters');
-              plugin.clearConditions(columnIndex);
-              plugin.filter();
-
-            },
-            child_name)
-
-        })
+            const plugin = hot.getPlugin('filters');
+            plugin.clearConditions(columnIndex);
+            plugin.filter();
+          },
+          child_name
+        );
+      });
     });
     // TODO: preserve memory of selection between tabs! in DH? => using outsideClickDeselects: false,  // for maintaining selection between tabs
-
-  })
+  });
 
   return data_harmonizers;
 }
