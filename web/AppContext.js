ea39bc7f4e907c865aa7f6171fc3979722c069f3
Kenneth Bruskiewicz <kbruskie@sfu.ca>
2024-07-22 15:06:04 -0700

    remove console log statements

--- web/AppContext.js
+++ web/AppContext.js
@@ -307,7 +307,7 @@ export class AppContext {
 
     // Handle local template refresh case
     if (isSameTemplatePath && isUploadedTemplate) {
-      console.log('Handle local template refresh case', 'isSameTemplatePath && isUploadedTemplate', template_path, this.dhs)
+      // console.log('Handle local template refresh case', 'isSameTemplatePath && isUploadedTemplate', template_path, this.dhs)
       this.clearInterface();
       return this.setupDataHarmonizers({
         data_harmonizers: this.dhs,
@@ -316,7 +316,7 @@ export class AppContext {
         forcedSchema: this.template.default.schema,
       });
     } else if (isSameTemplatePath) {
-      console.log('Handle local template refresh case', 'isSameTemplatePath', template_path, this.dhs)
+      // console.log('Handle local template refresh case', 'isSameTemplatePath', template_path, this.dhs)
       this.clearInterface();
       return this.setupDataHarmonizers({
         template_path,
@@ -326,7 +326,7 @@ export class AppContext {
 
     // Handle forced schema case
     if (isForcedSchemaProvided) {
-      console.log('Handle forced schema case', 'isForcedSchemaProvided')
+      // console.log('Handle forced schema case', 'isForcedSchemaProvided')
       this.appConfig = new AppConfig(template_path);
       this.clearInterface();
       return this.setupDataHarmonizers({
@@ -339,7 +339,7 @@ export class AppContext {
 
     // Handle changes in template path or locale
     if (!isSameTemplatePath || overrides.locale !== null) {
-      console.log('Handle changes in template path or locale', '!isSameTemplatePath || overrides.locale !== null')
+      // console.log('Handle changes in template path or locale', '!isSameTemplatePath || overrides.locale !== null')
       this.appConfig = new AppConfig(template_path);
       this.clearInterface();
       return this.setupDataHarmonizers({
@@ -363,7 +363,6 @@ export class AppContext {
 
   setDataHarmonizers(data_harmonizers) {
     this.dhs = data_harmonizers;
-    console.log('setDataHarmonizers', data_harmonizers, this.dhs)
     // NOTE: non-deterministic, assumes that the insertion order is the right order
     this.setCurrentDataHarmonizer(Object.keys(this.dhs)[0]);
   }
@@ -887,7 +886,6 @@ export class AppContext {
                 index === 0
               );
               dhTab.addEventListener('click', () => {
-                console.log('addEventListener', context);
                 // set the current dataharmonizer tab in the context
                 context.setCurrentDataHarmonizer(spec.name);
 
@@ -901,7 +899,6 @@ export class AppContext {
 
               // Different classes have slots allocated to them.
               const slotsForClass = findSlotNamesForClass(schema, cls_key);
-              console.log(slotsForClass);
 
               // idempotent
               // running this over the same initialization twice is expensive but shouldn't lose state
@@ -1018,8 +1015,6 @@ export class AppContext {
           _export_formats
         ),
       };
-
-      console.log('setupDataHarmonizers', data_harmonizers, dhs)
 
       // HACK
       context.setDataHarmonizers(dhs);
