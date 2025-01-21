66f96f39bfb97b9f0b2ccc820f1be3f2a94db533
Damion Dooley <damion_dooley@sfu.ca>
2025-01-21 02:09:10 -0800

    simplified menu system
    Eliminates need for webpack.config.js DirectoryTreePlugin() and manifest.json

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = (env, argv) => {
       schemas: 'schemas',
     },
     plugins: [
-      // necessary for templates.js
+      /* necessary for templates.js
       new DirectoryTreePlugin({
         dir: './web/templates',
         path: './web/templates/manifest.json',
@@ -37,10 +37,11 @@ module.exports = (env, argv) => {
           item.path = item.path.replace('web', '');
         },
       }),
+      */
       new HtmlWebpackPlugin({
         template: './index.html',
       }),
-      new CopyPlugin({
+      new CopyPlugin({ // Covers all schema.json including locales
         patterns: [
           {
             context: 'templates',
