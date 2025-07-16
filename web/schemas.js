f81872ab206d4f09f1d6c093f0a2adee8df5de3a
Damion Dooley <damion_dooley@sfu.ca>
2025-07-16 12:57:42 -0700

    switch to multilingual translation within schema

--- web/schemas.js
+++ web/schemas.js
@@ -1,6 +1,7 @@
 import menu_ from './templates/menu.json';
 
 export const menu = menu_;
+// The build process encodes all templates/ schema.son into one file.
 export const getSchema = async (schema_folder) => {
   return (await import(`./templates/${schema_folder}/schema.json`)).default;
 };
